3074ab2dbdc23291746c87d79a9f71741d9e4783
EyadElshaer <rarity-prowls-02@icloud.com>
2025-04-04 14:05:30 +0200

    beta 19

--- custom_components/buspro/const.py
+++ custom_components/buspro/const.py
@@ -4,3 +4,24 @@ CONF_PORT = "port"
 CONF_CHANNEL_ID = "channel_id"
 CONF_DEVICE_ID = "device_id"
 CONF_SUBNET_ID = "subnet_id"
+CONF_DEVICE_TYPE = "device_type"
+CONF_RUNNING_TIME = "running_time"
+CONF_NAME = "name"
+CONF_DIMMABLE = "dimmable"
+CONF_DEVICES = "devices"
+CONF_PLATFORM = "platform"
+
+# Device types
+DEVICE_TYPE_LIGHT = "light"
+DEVICE_TYPE_SWITCH = "switch"
+DEVICE_TYPE_SENSOR = "sensor"
+DEVICE_TYPE_BINARY_SENSOR = "binary_sensor"
+DEVICE_TYPE_CLIMATE = "climate"
+
+DEVICE_TYPES = [
+    DEVICE_TYPE_LIGHT,
+    DEVICE_TYPE_SWITCH,
+    DEVICE_TYPE_SENSOR,
+    DEVICE_TYPE_BINARY_SENSOR,
+    DEVICE_TYPE_CLIMATE
+]
