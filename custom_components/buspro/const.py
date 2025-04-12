1802de74ae192020bbbc83ec5a697a720517ffac
EyadElshaer <rarity-prowls-02@icloud.com>
2025-04-12 13:11:37 +0200

    beta 21

--- custom_components/buspro/const.py
+++ custom_components/buspro/const.py
@@ -2,28 +2,3 @@ DOMAIN = "buspro"
 CONF_HOST = "host"
 CONF_PORT = "port"
 DEFAULT_PORT = 6000
-
-# Device configuration constants
-CONF_SUBNET_ID = "subnet_id"
-CONF_DEVICE_ID = "device_id"
-CONF_CHANNEL = "channel"
-CONF_DEVICE_TYPE = "device_type"
-CONF_NAME = "name"
-
-# Cover specific constants
-CONF_OPENING_TIME = "opening_time"
-DEFAULT_OPENING_TIME = 20
-CONF_ADJUSTABLE = "adjustable"
-DEFAULT_ADJUSTABLE = True
-
-# Available device types
-DEVICE_TYPES = [
-    "cover",
-    "light",
-    "climate",
-    "switch",
-    "fan",
-    "sensor",
-    "universal_switch",
-    "scene",
-]
